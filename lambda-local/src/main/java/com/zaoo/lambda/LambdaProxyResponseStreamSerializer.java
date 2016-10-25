1d97d7331be2f51bf9bfae37e82f51d2869083aa
Yenwen Feng <tempofeng@gmail.com>
2016-10-25 20:30:37 +0800

    only output LambdaProxyResponse.body() to HttpServletResponse

--- lambda-local/src/main/java/com/zaoo/lambda/LambdaProxyResponseStreamSerializer.java
+++ lambda-local/src/main/java/com/zaoo/lambda/LambdaProxyResponseStreamSerializer.java
@@ -1,13 +1,18 @@
 package com.zaoo.lambda;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LambdaProxyResponseStreamSerializer implements LambdaStreamResponseSerializer {
+    private static final String UTF_8 = "UTF-8";
+    private final ObjectMapper objectMapper = ObjectMappers.getInstance();
+
     @Override
     public void deserialize(byte[] output, HttpServletResponse resp) throws IOException {
-        IOUtils.write(output, resp.getOutputStream());
+        LambdaProxyResponse response = objectMapper.readValue(output, LambdaProxyResponse.class);
+        IOUtils.write(response.getBody(), resp.getOutputStream(), UTF_8);
     }
 }
