f069445fbc1ffdb2f2f73379ef77be2491169d54
Yenwen Feng <tempofeng@gmail.com>
2016-10-26 10:27:26 +0800

    output statusCode and headers in LambdaProxyResponse

--- lambda-local/src/main/java/com/zaoo/lambda/LambdaProxyResponseStreamSerializer.java
+++ lambda-local/src/main/java/com/zaoo/lambda/LambdaProxyResponseStreamSerializer.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.IOUtils;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class LambdaProxyResponseStreamSerializer implements LambdaStreamResponseSerializer {
     private static final String UTF_8 = "UTF-8";
@@ -13,6 +14,10 @@ public class LambdaProxyResponseStreamSerializer implements LambdaStreamResponse
     @Override
     public void deserialize(byte[] output, HttpServletResponse resp) throws IOException {
         LambdaProxyResponse response = objectMapper.readValue(output, LambdaProxyResponse.class);
+        resp.setStatus(response.getStatusCode());
+        for (Map.Entry<String, String> entry : response.getHeaders().entrySet()) {
+            resp.addHeader(entry.getKey(), entry.getValue());
+        }
         IOUtils.write(response.getBody(), resp.getOutputStream(), UTF_8);
     }
 }
